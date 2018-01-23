7bc392f198755580493afd5d872a9baaa410d919
Ahmet Tunçel <ahmet.tuncel@outlook.com>
2018-01-23 07:59:05 +0300

    Adding Documantation

--- mssqlcon/R/mssqlcon.R
+++ mssqlcon/R/mssqlcon.R
@@ -1,7 +1,13 @@
-library(RODBC)
-
+devtools::use_package("RODBC")
+#' MSSQL db Connection
+#'
+#' This function connect MSSQL db.
+#' @keywords MSSQL
+#' @examples mssqlcon(serverName, dbname)
+#' @title mssqlCon
+#' @export
 mssqlcon <-function(serverName, dbname){
-        
+
         return(paste('driver={SQL Server};server=', serverName, ';database=',dbname, ';trusted_connection=true'))
-        
+
 }
